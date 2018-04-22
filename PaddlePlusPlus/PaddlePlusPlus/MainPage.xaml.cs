07bd5e26b3cbc9903977f4d78a3f0f1f3c66e85e
Rhodri Mativo <rmativo3@ncstudents.niagaracollege.ca>
2018-04-22 17:33:44 -0400

    got the keydown event working and setup character enum

--- PaddlePlusPlus/PaddlePlusPlus/MainPage.xaml.cs
+++ PaddlePlusPlus/PaddlePlusPlus/MainPage.xaml.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.System;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -12,6 +14,9 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+
+
+
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -22,9 +27,45 @@ namespace PaddlePlusPlus
     /// </summary>
     public sealed partial class MainPage : Page
     {
+
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+
+
+
+
+        StringBuilder inputDisplay = new StringBuilder("");
+
+
+
+        private void txtInput_PreviewKeyDown(object sender, KeyRoutedEventArgs e)
+        {
+            if (e.Key == VirtualKey.Right)
+            {
+                inputDisplay.Clear();
+
+            }
+            else if (e.Key == VirtualKey.Z)
+            {
+                inputDisplay.Append("Z");
+
+            }
+            else if (e.Key == VirtualKey.X)
+            {
+                inputDisplay.Append("X");
+
+            }
+            else
+            {
+
+            }
+
+
+            txtInput.Text = inputDisplay.ToString();
+
         }
     }
 }
---
+++ PaddlePlusPlus/PaddlePlusPlus/Utilities/Characters.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaddlePlusPlus.Utilities
+{
+    public enum Ch
+    {
+        A,
+        B,
+        C,
+        D,
+        E,
+        F,
+        G,
+        H,
+        I,
+        J,
+        K,
+        L,
+        M,
+        N,
+        O,
+        P,
+        Q,
+        R,
+        S,
+        T,
+        U,
+        V,
+        W,
+        X,
+        Y,
+        Z,
+        _,
+        other,
+
+    }
+
+
+    public static class KeyInput
+    {
+        
+
+
+        public static Ch InputToCh(string input)
+        {
+
+            switch (input)
+            {
+                case "zx":
+                    return Ch.A;
+
+                case "xzzz":
+                    return Ch.B;
+
+                case "xzxz":
+                    return Ch.C;
+                    
+                case "xzz":
+                    return Ch.D;
+                    
+                case "z":
+                    return Ch.E;
+                    
+                case "zzxz":
+                    return Ch.F;
+                    
+                case "xxz":
+                    return Ch.G;
+                    
+                case "zzzz":
+                    return Ch.H;
+                    
+                case "zz":
+                    return Ch.I;
+                    
+                case "zxxx":
+                    return Ch.J;
+                    
+                case "xzx":
+                    return Ch.K;
+                    
+                case "zxzz":
+                    return Ch.L;
+                    
+                case "xx":
+                    return Ch.M;
+                    
+                case "xz":
+                    return Ch.N;
+                    
+                case "xxx":
+                    return Ch.O;
+                    
+                case "zxxz":
+                    return Ch.P;
+                    
+                case "xxzx":
+                    return Ch.Q;
+                    
+                case "zxz":
+                    return Ch.R;
+                    
+                case "zzz":
+                    return Ch.S;
+                    
+                case "x":
+                    return Ch.T;
+                    
+                case "zzx":
+                    return Ch.U;
+                    
+                case "zzzx":
+                    return Ch.V;
+                    
+                case "zxx":
+                    return Ch.W;
+                    
+                case "xzzx":
+                    return Ch.X;
+                    
+                case "xzxx":
+                    return Ch.Y;
+                    
+                case "xxzz":
+                    return Ch.Z;
+                    
+                case " ":
+                    return Ch._;
+                    
+                default:
+                    return Ch.other;
+
+            }
+
+        }
+
+        public static bool isValid(Ch input)
+        {
+            if (input != Ch.other)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+           
+        }
+
+
+
+    }
+
+
+
+}
